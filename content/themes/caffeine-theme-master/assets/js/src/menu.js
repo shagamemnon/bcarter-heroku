48ab73191d9efb92f511311c0463387938b7274d
Franklin Taylor <Frank@Dino.local>
2017-01-26 03:08:40 -0800

    updated mobile styles

--- content/themes/caffeine-theme-master/assets/js/src/menu.js
+++ content/themes/caffeine-theme-master/assets/js/src/menu.js
@@ -2,4 +2,8 @@ $(function(){
   $('i.fa-search').click(function(){
     $(this).children('span').toggle();
   });
+
+  $('.js-slideout-toggle').on('click', function() {
+    $('body, .panel, .slideout-menu, .tags-button, .menu').toggleClass('nav-active');
+  });
 });
