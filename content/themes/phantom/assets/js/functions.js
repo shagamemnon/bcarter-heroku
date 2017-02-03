983dc7c4aa8fb25a9d5a67eaac9ea45b0155e490
Franklin Taylor <Frank@Dino.local>
2017-02-02 23:35:22 -0800

    added toastr

--- content/themes/phantom/assets/js/functions.js
+++ content/themes/phantom/assets/js/functions.js
@@ -6,4 +6,11 @@ $(function(){
   $('.js-slideout-toggle').on('click', function() {
     $('body, .panel, .slideout-menu, .tags-button, .menu').toggleClass('nav-active');
   });
+toastr.options.showMethod = 'slideDown';
+toastr.options.hideMethod = 'slideUp';
+toastr.options.closeMethod = 'slideUp';
+toastr.options.preventDuplicates = true;
+toastr.options.progressBar = true;
+toastr.options.escapeHtml = false;
+toastr.options.hideEasing = 'linear';
 });
